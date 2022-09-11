d23b81e24e3422ebeae7801a7253bfc0a8128bc1
Vlad Dekterev <vlad.decterev@icloud.com>
2022-09-11 14:37:23 +0400

    Задание 2.1.7

---
+++ task_2_1_7.py
@@ -0,0 +1,20 @@
+class Line:
+    def __init__(self, x1: int, y1: int, x2: int, y2: int):
+        self.__x1, self.__y1 = x1, y1
+        self.__x2, self.__y2 = x2, y2
+
+    def set_coords(self, x1: int, y1: int, x2: int, y2: int):
+        self.__x1, self.__y1 = x1, y1
+        self.__x2, self.__y2 = x2, y2
+
+    def get_coords(self):
+        return self.__x1, self.__y1, self.__x2, self.__y2
+
+    def draw(self):
+        print(*[self.__x1, self.__y1, self.__x2, self.__y2])
+
+
+
+l = Line(1, 2, 3, 4)
+print(l.get_coords())
+l.draw()
