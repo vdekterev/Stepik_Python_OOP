0b49e9ee2f6dd0d90ef2c5fc4c60bd850114442d
Vlad Dekterev <vlad.decterev@icloud.com>
2022-09-12 13:18:30 +0400

    Задание 2.2.4

---
+++ task_2_2_4.py
@@ -0,0 +1,12 @@
+class Car:
+    def __init__(self):
+        self.__model = None
+
+    @property
+    def model(self):
+        return self.__model
+
+    @model.setter
+    def model(self, model: str):
+        if type(model) == str and 2 <= len(model) <= 100:
+            self.__model = model
