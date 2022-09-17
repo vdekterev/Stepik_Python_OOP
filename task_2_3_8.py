5c14d2cb1edbd7d601c2e64e1ad46678c6e38b1b
Vlad Dekterev <vlad.decterev@icloud.com>
2022-09-17 08:40:42 +0400

    Задание 2.3.8

---
+++ task_2_3_8.py
@@ -0,0 +1,53 @@
+class StringValue:
+    def __init__(self, min_length, max_length):
+        self.min_length = min_length
+        self.max_length = max_length
+
+    def __set_name__(self, owner, name):
+        self.name = '_' + name
+
+    def __get__(self, instance, owner):
+        return getattr(instance, self.name)
+
+    def __set__(self, instance, value):
+        if type(value) == str and self.min_length <= len(value) <= self.max_length:
+            setattr(instance, self.name, value)
+
+
+class PriceValue:
+    def __init__(self, max_value):
+        self.max_value = max_value
+
+    def __set_name__(self, owner, name):
+        self.name = '_' + name
+
+    def __get__(self, instance, owner):
+        return getattr(instance, self.name)
+
+    def __set__(self, instance, value):
+        if type(value) in (int, float) and value <= self.max_value:
+            setattr(instance, self.name, value)
+
+
+class SuperShop:
+    def __init__(self, name):
+        self.name = name
+        self.goods = []
+
+    def add_product(self, product):
+        self.goods.append(product)
+
+    def remove_product(self, product):
+        try:
+            self.goods.remove(product)
+        except ValueError:
+            print('Такого товара нет в магазине')
+
+
+class Product:
+    name = StringValue(2, 50)
+    price = PriceValue(10000)
+
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
