7c129f07d5f3f0dd45c605db98c7b553d9640c62
Vlad Dekterev <vlad.decterev@icloud.com>
2022-09-19 14:24:46 +0400

    Задание 3.2.4

---
+++ task_3_2_4.py
@@ -0,0 +1,40 @@
+from string import ascii_lowercase, digits
+
+
+class LoginForm:
+    def __init__(self, name, validators=None):
+        self.name = name
+        self.validators = validators
+        self.login = ""
+        self.password = ""
+
+    def post(self, request):
+        self.login = request.get('login', "")
+        self.password = request.get('password', "")
+
+    def is_validate(self):
+        if not self.validators:
+            return True
+
+        for v in self.validators:
+            if not v(self.login) or not v(self.password):
+                return False
+
+        return True
+
+
+class LengthValidator:
+    def __init__(self, min_length, max_length):
+        self.min_length = min_length
+        self.max_length = max_length
+
+    def __call__(self, string, *args, **kwargs):
+        return self.min_length <= len(string) <= self.max_length
+
+
+class CharsValidator:
+    def __init__(self, chars):
+        self.chars = chars
+
+    def __call__(self, string, *args, **kwargs):
+        return all(map(lambda x: x in self.chars, string))
