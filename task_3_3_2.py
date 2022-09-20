d056debc7fc7407deaf5b47c90501da733b1a3d4
Vlad Dekterev <vlad.decterev@icloud.com>
2022-09-20 14:07:25 +0400

    Задание 3.3.2

---
+++ task_3_3_2.py
@@ -0,0 +1,8 @@
+class Book:
+    def __init__(self, title, author, pages):
+        self.title = title
+        self.author = author
+        self.pages = pages
+
+    def __str__(self):
+        return f'Книга: {self.title}; {self.author}; {self.pages}'
