2820fe2b92becee6fbff0a7c700e347d836247c2
Vlad Dekterev <vlad.decterev@icloud.com>
2022-09-20 08:25:43 +0400

    Задание 3.2.7

--- task_3_2_7.py
+++ task_3_2_7.py
@@ -3,6 +3,10 @@ class HandlerGET:
         self.__func = func
 
     def __call__(self, request, *args, **kwargs):
+        return self.get(self.__func, request)
+
+    def get(self, func, request, *args, **kwargs):
         if 'method' in request.keys() and request['method'] != 'GET':
             return
         return f'GET: {self.__func(request)}'
+
