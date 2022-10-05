a333cce64dc1fa9370684121deed3a0ad4fc94d3
Vlad Dekterev <vlad.decterev@icloud.com>
2022-10-05 12:30:43 +0400

    Задание 3.6.4

---
+++ task_3_6_4.py
@@ -0,0 +1,12 @@
+class Rect:
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+
+    def __hash__(self):
+        return hash((self.width, self.height))
+
+    def __eq__(self, other):
+        return self.width == other.width and self.height == other.height
