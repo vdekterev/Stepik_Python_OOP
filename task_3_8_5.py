bf069e748d605f5eb97132123e0035206bc6ea50
Vlad Dekterev <vlad.decterev@icloud.com>
2022-11-24 13:41:22 +0400

    Задание 3.8.5

--- task_3_8_5.py
+++ task_3_8_5.py
@@ -2,36 +2,34 @@ class IntegerValue:
     def __set_name__(self, owner, name):
         self.name = '_' + name
 
+    def __get__(self, instance, owner):
+        return instance.__dict__[self.name]
+
     def __set__(self, instance, value):
         if type(value) != int:
             raise ValueError('возможны только целочисленные значения')
-        setattr(instance, self.name, value)
-
-    def __get__(self, instance, owner):
-        return getattr(instance, self.name)
+        instance.__dict__[self.name] = value
 
 
 class CellInteger:
     value = IntegerValue()
 
     def __init__(self, start_value=0):
-        self.start_value = start_value
+        self.value = start_value
 
 
 class TableValues:
-    def __init__(self, rows: int, cols: int, cell=None):
-        self.rows = rows if type(rows) == int else 0
-        self.cols = cols if type(cols) == int else 0
-        if cell is None:
+    def __init__(self, rows, cols, cell=None):
+        if not cell:
             raise ValueError('параметр cell не указан')
+        self._rows = rows
+        self._cols = cols
         self.cells = tuple(tuple(cell() for _ in range(cols)) for _ in range(rows))
 
-    def __check_index(self, value):
-        col, row = value[0], value[1]
-        if type(col) != int or type(row) != int:
-            raise ValueError('col and row must be integers!')
-        elif not(0 <= col <= self.cols and 0 <= row <= self.rows):
-            raise IndexError('Index out of range!')
+    def __check_index(self, index):
+        r, c = index
+        if type(r) != int or not (0 <= r <= self._rows) or type(c) != int or not (0 <= c <= self._cols):
+            raise IndexError('Неверный индекс')
 
     def __getitem__(self, item):
         self.__check_index(item)
@@ -39,10 +37,4 @@ class TableValues:
 
     def __setitem__(self, key, value):
         self.__check_index(key)
-        if type(value) != int:
-            raise ValueError('возможны только целочисленные значения')
-        self.cells[key[0]][key[1]] = value
-
-
-table = TableValues(2, 3, cell=CellInteger)
-table[0, 1] = 111
+        self.cells[key[0]][key[1]].value = value
